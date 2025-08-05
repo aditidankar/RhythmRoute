aa660e31a6787e4f40f5e65d2c87d0873f63f624
Aditi Dankar <aditidankar@gmail.com>
2025-08-05 19:30:13 +0100

    chore: print hyperparameters for logging

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from args import parse_train_opt
 from EDGE import EDGE
+import torch
 
 
 def train(opt):
@@ -9,4 +10,21 @@ def train(opt):
 
 if __name__ == "__main__":
     opt = parse_train_opt()
+    
+    # Print PyTorch GPU diagnostics
+    print("---------- PyTorch GPU Diagnostics ----------")
+    print(f"Is CUDA available?          : {torch.cuda.is_available()}")
+    print(f"Number of GPUs PyTorch sees : {torch.cuda.device_count()}")
+    if torch.cuda.is_available():
+        print(f"Current device ID       : {torch.cuda.current_device()}")
+        print(f"Current device name     : {torch.cuda.get_device_name(torch.cuda.current_device())}")
+    print("---------------------------------------------")
+
+    # Print the hyperparameters for logging
+    print("-------------- Hyperparameters --------------")
+    # vars(opt) converts the argparse.Namespace object to a dictionary
+    for k, v in vars(opt).items():
+        print(f"{k:<20}: {v}")
+    print("----------------------------------------------")
+
     train(opt)
